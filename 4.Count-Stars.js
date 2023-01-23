fd11e4a7f42f16b34932768d2551444358ac07a8
rismanharism <risman.haris6@gmail.com>
2023-01-23 16:30:02 +0700

    1

--- 4.Count-Stars.js
+++ 4.Count-Stars.js
@@ -1,54 +1,59 @@
-function countStars(stars) {
-  const result = {
-    '*****': 0,
-    '****': 0,
-    '***': 0,
-    '**': 0,
-    '*': 0,
-  }
+// function countStars(stars) {
+//   const result = {
+//     '*****': 0,
+//     '****': 0,
+//     '***': 0,
+//     '**': 0,
+//     '*': 0,
+//   }
 
-  for (const starsArray of stars) {
-    let starCount = 0;
-    for (const star of starsArray) {
-      if (star === '*') {
-        starCount++;
-      }
-    }
-    if (starCount === 5) {
-      result["*****"]++;
-    }
-    if (starCount === 4) {
-      result["****"]++;
-    }
-    if (starCount === 3) {
-      result["***"]++;
-    }
-    if (starCount === 2) {
-      result["**"]++;
-    }
-    if (starCount === 1) {
-      result["*"]++;
-    }
-  }
+//   for (const starsArray of stars) {
+//     let starCount = 0;
+//     for (const star of starsArray) {
+//       if (star === '*') {
+//         starCount++;
+//       }
+//     }
+//     if (starCount === 5) {
+//       result["*****"]++;
+//     }
+//     if (starCount === 4) {
+//       result["****"]++;
+//     }
+//     if (starCount === 3) {
+//       result["***"]++;
+//     }
+//     if (starCount === 2) {
+//       result["**"]++;
+//     }
+//     if (starCount === 1) {
+//       result["*"]++;
+//     }
+//   }
 
-  return result;
-}
-
-// Instructor Version
-// function sumColumn(arr) {
-// 	// Tulis kode disini
-// 	let result = [];
-// 	for (let i = 0; i < arr.length; i++) {
-// 	    for (let j = 0; j < arr[i].length; j++) {
-// 	        if (result[j] !== undefined) {
-// 	            result[j] += arr[i][j];
-// 	        } else {
-// 	            result[j] = arr[i][j];
-// 	        }
-// 	    }
-// 	}
-// 	return result;
+//   return result;
 // }
+
+function countStars(arr) {
+	// Tulis kode disini
+	let result = {
+	    '*****': 0,
+	    '****': 0,
+	    '***': 0,
+	    '**': 0,
+	    '*': 0
+	};
+	for (let i = 0; i < arr.length; i++) {
+	    let totalStars = '';
+	    for (let j = 0; j < arr[i].length; j++) {
+	        if (arr[i][j] === '*') {
+	            totalStars += '*';
+	        }
+	    }
+	    result[totalStars]++;
+	}
+	return result;
+}
 
 console.log(
   countStars([
